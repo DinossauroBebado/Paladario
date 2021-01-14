ab0ad6597cb2f707810369caf81a38e6b7a46875
DinossauroBebado <guipsilva32@gmail.com>
2021-01-14 15:55:15 -0300

    DHT11 sensor add

--- Paladario.ino
+++ Paladario.ino
@@ -3,6 +3,7 @@
 //-----Autor:@dinossauro.bebado/@DinossauroBbad1                                     ---------
 //-----Objetivo :Ter controle de sensores, luzes e equipamentos do paladario         ---------
 //-----atraves da internet                                                           ---------
+//-----data : ... 12/01/21 - dht11
 //--------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------
 //--------------------------------------------------------------------------------------------
@@ -28,9 +29,11 @@
 #include <WiFiUdp.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <dht11.h>
+
 //-------------------Wi-fi Config--------------------------#
-const char *ssid     = "Torta";
-const char *password = "3141592653589";
+const char *ssid     = "DinossauroBebado";
+const char *password = ;
 //-------------------NTP Config----------------------------#
 const long utcOffsetInSeconds = -10800;
 char daysOfTheWeek[7][12] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sabado"};
@@ -46,13 +49,18 @@ LiquidCrystal_I2C lcd(endereco, colunas, linhas);
 //-------------------Definição de pinos--------------------#
 const int bomba = D3 ; 
 const int luz = D4 ; 
+const int DHT11 = D5;
 //-------------------Declaração de variaveis---------------#
 int hora;
 int minutos;
 int segundo;
+int TEMPERATURA;
+int HUMIDADE ;
 // Define o cliente NTP para ter o tempo
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds);
+// Define o sensor dht11 de temeperatura e humidade 
+dht11 SensorHT;
 
 void setup(){
   Serial.begin(9600);
@@ -69,7 +77,7 @@ void setup(){
     delay ( 500 );
     Serial.print ( "." );
   }
-  lcd.println("Paladario Conectado");
+  lcd.println("Conectado");
   timeClient.begin();
   //---------------
   pinMode(luz,OUTPUT);
@@ -86,7 +94,7 @@ void loop() {
   hora = timeClient.getHours();
   minutos = timeClient.getMinutes();
   segundo = timeClient.getSeconds();
-  lcdDisplay(hora,minutos,segundo,hora_ligar,hora_desligar,false);
+  lcdDisplay(hora,minutos,segundo,hora_ligar,hora_desligar,false,SensorT(),SensorH());
   Serial.println(hora);
   Serial.println("Paladario desligado");
   //----------------------Codigo para as luzes ligadas 
@@ -99,7 +107,7 @@ void loop() {
   hora = timeClient.getHours();
   minutos = timeClient.getMinutes();
   segundo = timeClient.getSeconds();
-  lcdDisplay(hora,minutos,segundo,hora_ligar,hora_desligar,true);
+  lcdDisplay(hora,minutos,segundo,hora_ligar,hora_desligar,true,SensorT(),SensorH());
   delay(1000);
   }
   
@@ -108,7 +116,7 @@ void loop() {
 }
 
 
-void lcdDisplay(int horas,int minutos,int segundos,int  hora_ligar,int hora_desligar, bool light)
+void lcdDisplay(int horas,int minutos,int segundos,int  hora_ligar,int hora_desligar, bool light,int TEMPERATURA,int HUMIDADE)
 { //monstra a hora e quando o timer liga e desliga
   lcd.clear(); 
   lcd.setCursor(0,0);//linha de cima 
@@ -117,16 +125,38 @@ void lcdDisplay(int horas,int minutos,int segundos,int  hora_ligar,int hora_desl
   lcd.print("--->");
   lcd.print("Off:");
   lcd.print(hora_desligar); 
-  lcd.setCursor(0,1); //linha debaixo 
-  lcd.print("-----");
-  lcd.print(horas);
-  lcd.print(":");
-  lcd.print(minutos);
-  lcd.print(":");
-  lcd.print(segundos);
-  lcd.print("-----");
+//------------------------------------------
+//----------------------------------------------
+lcd.setCursor(0,1); //linha debaixo 
+lcd.print("|");
+lcd.print(horas);
+lcd.print(":");
+lcd.print(minutos);
+lcd.print(":");
+lcd.print(segundos);
+lcd.print("|");
+//----------------------------------------------
+lcd.print(TEMPERATURA);
+lcd.print("C");
+lcd.print("|");
+//----------------------------------------------
+lcd.print(HUMIDADE);
+lcd.print("%");
   if(light){lcd.backlight();//liga ou desliga as luzes do LCD 
   }
   else{lcd.noBacklight();
     }
+  }
+
+int SensorH()
+{
+  int chk = SensorHT.read(DHT11);
+  HUMIDADE = ((float)SensorHT.humidity);
+  return HUMIDADE ; 
+  }
+int SensorT()
+{
+  int chk = SensorHT.read(DHT11);
+  TEMPERATURA = ((float)SensorHT.temperature);
+  return TEMPERATURA ; 
   }
